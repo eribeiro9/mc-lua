528df8379e41fb4352341c7af39c704551c06711
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2017-04-13 13:34:11 -0400

    Refactoring

--- chop2x2.lua
+++ chop2x2.lua
@@ -1,33 +1,29 @@
 robot = require("robot")
 
-function chopTree()
-  local height = 1
-
-  while robot.detectUp() do
-    robot.swingUp()
-    robot.up()
-    robot.swing()
-    height = height + 1
-  end
-
-  robot.turnLeft()
-  robot.swing()
-  robot.forward()
-  robot.turnRight()
-  robot.swing()
-
-  for 1, height do
-    robot.swingDown()
-    robot.down()
-    robot.swing()
-  end
-end
-
 robot.swing()
 robot.forward()
 robot.swing()
 
-chopTree()
+local height = 1
+
+while robot.detectUp() do
+  robot.swingUp()
+  robot.up()
+  robot.swing()
+  height = height + 1
+end
+
+robot.turnLeft()
+robot.swing()
+robot.forward()
+robot.turnRight()
+robot.swing()
+
+for 1, height do
+  robot.swingDown()
+  robot.down()
+  robot.swing()
+end
 
 robot.back()
 robot.turnRight()
