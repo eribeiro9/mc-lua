93b08c25babfdd9a3d59fe624e46375d10651dae
Eric Ribeiro <eric.james.ribeiro@gmail.com>
2017-04-13 13:25:35 -0400

    Decrease file size

--- tunnel.lua
+++ tunnel.lua
@@ -1,70 +1,46 @@
-local robot = require("robot")
+robot = require("robot")
 
--- Accept distance and round up to nearest even number
-local distance = ...
-distance = distance + distance % 2
+distance = ...
+distance = distance + distance % 2 -- round up to even number
 
--- Initalize robot position
-function readyPosition()
-  robot.forward()
-  robot.turnRight()
-  robot.forward()
-  robot.turnLeft()
-  robot.up()
-end
-
--- A single 3-tall column
 function nextStep()
   robot.swing()
   robot.forward()
   while robot.detectUp() do
     robot.swingUp()
-    -- TODO: wait for gravity
   end
   robot.swingDown()
 end
 
--- A single 3x3x1 layer
-function nextLayer(isEven)
-  nextStep()
-  if isEven then
-    robot.turnRight()
-  else
-    robot.turnLeft()
-  end
-  nextStep()
-  nextStep()
-  if isEven then
-    robot.turnLeft()
-  else
-    robot.turnRight()
-  end
-end
-
--- Mines 3 x 3 x distance tunnel
 function mineTunnel()
   for 1, distance, 2 do
-    nextLayer(false)
-    nextLayer(true)
+    nextStep()
+    robot.turnLeft()
+    nextStep()
+    nextStep()
+    robot.turnRight()
+
+    nextStep()
+    robot.turnRight()
+    nextStep()
+    nextStep()
+    robot.turnLeft()
   end
 end
 
--- Return to initial position
-function returnPosition()
-  robot.down()
-  robot.turnLeft()
-  robot.forward()
-  robot.turnLeft()
-  for 1, distance do
-    robot.forward()
-  end
-  robot.forward()
-end
+robot.forward()
+robot.turnRight()
+robot.forward()
+robot.turnLeft()
+robot.up()
 
-
-
--- Main application
--- TODO: Check if has enough fuel
-readyPosition()
 mineTunnel()
-returnPosition()
+
+robot.down()
+robot.turnLeft()
+robot.forward()
+robot.turnLeft()
+for 1, distance do
+  robot.forward()
+end
+robot.forward()
